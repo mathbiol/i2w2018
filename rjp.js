c636dd46c798f870d2fbc4af7c06ba1ea6c1f6e4
Piazza <Richard.Piazza@stonybrookmedicine.edu>
2018-09-12 14:45:39 -0400

    Add Function

--- rjp.js
+++ rjp.js
@@ -1,3 +1,5 @@
+
+// Git hub,  commit then push.  If others change the file then pull request 
 console.log('RJP is here yaaaaa')
 
 rjp=function(){}
@@ -5,3 +7,6 @@ rjp=function(){}
 rjp.date=Date()
 rjp.double=(x)=>2*x
 
+setTimeout(function() {
+    alert('hello');
+  }, 1000);
