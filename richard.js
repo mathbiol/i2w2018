95eef9a589a62d8bcf2861daacacf878aede8f37
rmoffitt <richard.a.moffitt@gmail.com>
2018-10-10 10:34:57 -0400

    richard

--- richard.js
+++ richard.js
@@ -10,4 +10,55 @@ richard.unzip = function(z){
     return u
 }
 
+
+richard.getSearchParms=function(){
+    var parms={}
+    location.search.slice(1)
+            .split('&')
+            .map(q=>{
+                return q.split('=')
+            }).forEach(av=>{
+                parms[av[0]]=av[1]
+            })
+    return parms
+
+}
+
+richard.loginGoogle=function(div){ // adds Google login process
+    var div = div||richard.div // pick default div or the one provided
+    if(div){
+        // build login button
+        div.innerHTML+='<div><button id="googleLoginButton">Login Google</button> <a href="'+location.origin+location.pathname+'">reset</a></div>'
+        googleLoginButton.onclick=richard.doLoginGoogle
+    }else{
+        console.log('no div where to assemble the login button, starting programatically')
+        richard.doLoginGoogle()
+    }
+    // redirected from oauth
+    var parms = richard.getSearchParms()
+    if(parms.code){
+        debugger
+    }
+    
+}
+
+richard.doLoginGoogle=function(){
+    console.log('richard click')
+}
+
+// ini
+window.onload=function(){
+    if(document.body.querySelector('#richardDiv')){
+       richard.div=document.body.querySelector('#richardDiv')
+       richard.loginGoogle()
+    }
+    if(document.body.querySelector('#jonasDiv')){
+       jonas.div=document.body.querySelector('#jonasDiv')
+       jonas.loginGithub()
+       jonas.loginGoogle()
+       //jonas.loginBox()
+       // Stony Brook: https://sbm-it.github.io/msdn/oauth2.html
+    }
+}
+
 console.log(' . . . loaded')
